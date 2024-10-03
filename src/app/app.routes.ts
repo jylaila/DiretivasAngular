f7e57851990271ad38962dd334f3da9f30daa5a6
Maria Janaina Silva Ferreira <Maria.Ferreira@fit-tecnologia.org.br>
2024-10-03 18:23:40 -0300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CourseComponent } from './course/course.component';
+import { CourseDescriptionComponent } from './course-description/course-description.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'course', component: CourseComponent },
+  { path: 'course-description', component: CourseDescriptionComponent },
+];
+
---
+++ src/app/course-description/course-description.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-description',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-description.component.html',
+  styleUrl: './course-description.component.css'
+})
+export class CourseDescriptionComponent {
+
+}
